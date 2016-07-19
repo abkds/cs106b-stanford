d88e625d71dd99afdb69eea7229228c77d4f66b2
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-07-20 00:16:41 +0530

    Implement stringToInt

--- Assignment1-linux/3-NumericConversion/NumericConversion.cpp
+++ Assignment1-linux/3-NumericConversion/NumericConversion.cpp
@@ -1,26 +1,61 @@
 /*
  * File: NumericConversion.cpp
  * ---------------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter project for the numeric-conversion problem
- * in which you implement the functions intToString and stringToInt.
- * [TODO: rewrite the documentation]
+ *
+ * This program implements function to convert
+ * integer to string and vice versa.
  */
 
 #include <iostream>
 #include <string>
-#include "console.h"
-using namespace std;
+#include <sstream>
 
 /* Function prototypes */
 
-string intToString(int n);
-int stringToInt(string str);
+std::string intToString(int n);
+int stringToInt(std::string str);
 
 /* Main program */
 
 int main() {
-   // [TODO: fill in the code]
-   return 0;
+    int n = -1729;
+    /* Outputs string concatenation result -17291 */
+    std::cout << intToString(n) + "1" << std::endl;
+
+    /* Outputs the integer sum between -1729 and 1 */
+    std::cout << stringToInt("-1729") + 1 << std::endl;
+
+    return 0;
+}
+
+
+/*
+ * Function: intToString
+ * Usage: str = intToString(9)
+ * ---------------------------
+ * Converts a integer to string by using stringstreams
+ * stringstreams takes in an integer and redirects the
+ * output as string.
+ */
+std::string intToString(int n) {
+    std::stringstream converter;
+    converter << n;
+    std::string result;
+    converter >> result;
+    return result;
+}
+
+/*
+ * Function: stringToInt
+ * Usage: value = stringToInt("-200")
+ * ----------------------------------
+ * Converts a string to an integer and returns it
+ * assumes that the string is a valid integer.
+ */
+int stringToInt(std::string str) {
+    std::stringstream converter;
+    converter << str;
+    int n;
+    converter >> n;
+    return n;
 }
