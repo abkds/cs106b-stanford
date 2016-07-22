91ebf0c41d74bbbedf1296c7b65c1213a6b6baa0
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-07-23 02:46:44 +0530

    Add I/O in WordLadder
    Implement the basic outline of WordLadder
    and function prototypes.

---
+++ Assignment-2/RandomWriter/RandomWriter.cpp
@@ -0,0 +1,24 @@
+/*
+ * File: RandomWriter.cpp
+ * ----------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the random writer problem
+ * on Assignment #2.
+ * [TODO: extend the documentation]
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+#include "map.h"
+#include "random.h"
+#include "strlib.h"
+#include "vector.h"
+using namespace std;
+
+int main() {
+	// [TODO: fill in the code]
+	return 0;
+}
---
+++ Assignment-2/WordLadder/WordLadder.cpp
@@ -0,0 +1,92 @@
+/*
+ * File: WordLadder.cpp
+ * --------------------
+ *
+ * This file implements a program to find the shortest ladder between
+ * two English words. EnglishWords.txt contains a lexicon of all the
+ * English words.
+ *
+ * A sample run of the program for source word [work] and destination
+ * word [play] would result in the shortest ladder as depicted:
+ *
+ *		 work fork form foam flam flay play
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include "lexicon.h"
+
+/* Function prototypes */
+std::vector<std::string> findLadder(std::string source, std::string destination,
+	Lexicon lexicon);
+
+template <typename T>
+void printVector(std::vector<T> vec, std::string separator);
+
+/* Main Program */
+int main() {
+
+	/* Load the lexicon */
+	Lexicon english("EnglishWords.txt");
+
+	std::string word = "xx";
+
+	/*for (char c0 = 'a'; c0 <= 'z'; c0++) {
+
+		word[0] = c0;
+		for (char c1 = 'a'; c1 <= 'z'; c1++) {
+
+			word[1] = c1;
+			if (english.contains(word)) {
+				std::cout << word << std::endl;
+			}
+		}
+	}*/
+
+	while (true) {
+		std::string source, destination;
+		std::cout << "Enter start word (RETURN to quit): ";
+
+		getline(std::cin, source);
+
+		if (source.size() == 0) {	/* RETURN has been entered */
+			std::cout << "GoodBye !" << std::endl;
+			break;
+		}
+
+		std::cout << "Enter the ending word: ";
+		getline(std::cin, destination);
+
+		std::cout << "Searching..." << std::endl;
+
+		if (english.contains(source) && english.contains(destination)) {
+			std::vector<std::string> ladder = findLadder(source, destination,
+				english);
+
+			std::cout << "Ladder found: ";
+			printVector(ladder, " -> ");
+			std::cout << std::endl;
+		} else {
+			std::cout << "No word ladder could be find." << std::endl;
+		}
+
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
+
+std::vector<std::string> findLadder(std::string source, std::string destination,
+	Lexicon lexicon) {
+	std::vector<std::string> ladder;
+	return ladder;
+}
+
+template <typename T>
+void printVector(std::vector<T> vec, std::string separator) {
+	return;
+}
