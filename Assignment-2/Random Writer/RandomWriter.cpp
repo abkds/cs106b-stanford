68e3aa4f7e297a0ebca392e63730f3bf17f07146
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-07-22 06:49:59 +0530

    Add starter files for P2

---
+++ Assignment-2/Random Writer/RandomWriter.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: RandomWriter.cpp
+ * ----------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Random Writer problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    // [TODO: fill in the code]
+    return 0;
+}
---
+++ Assignment-2/Word Ladder/WordLadder.cpp
@@ -0,0 +1,19 @@
+/*
+ * File: WordLadder.cpp
+ * ----------------------
+ *
+ * This program finds a shortest word ladder between two 
+ * words present in a given lexicon, using breadth-first
+ * search strategy. "lexicon.h" from Stanford CPP library 
+ * has been used.
+ */
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include "lexicon.h"
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
