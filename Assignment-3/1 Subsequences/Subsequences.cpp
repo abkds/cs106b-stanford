bf70c8b1437ad4b2c693f65f56a94d4adfff3646
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-07-26 08:42:01 +0530

    Implement Subsequence function

--- Assignment-3/1 Subsequences/Subsequences.cpp
+++ Assignment-3/1 Subsequences/Subsequences.cpp
@@ -1,23 +1,63 @@
 /*
  * File: Subsequences.cpp
  * ----------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
  * This file is the starter project for the Subsequences problem
  * on Assignment #3.
- * [TODO: extend the documentation]
  */
 
 #include <iostream>
 #include <string>
-#include "console.h"
-using namespace std;
 
 /* Given two strings, returns whether the second string is a
  * subsequence of the first string.
  */
-bool isSubsequence(string text, string subsequence);
+bool isSubsequence(std::string text, std::string subsequence);
 
 int main() {
-	// [TODO: Fill this in! ]]
+    std::string text, subsequence;
+
+    std::cout << "Enter the text: ";
+    getline(std::cin, text);
+    std::cout << "Enter the subsequence to search for: ";
+    getline(std::cin, subsequence);
+
+	std::cout << "Is \"" << subsequence << "\" a subsequence of \"" << text
+    << "\" ? : " << std::boolalpha << isSubsequence(text, subsequence);
+    std::cout << std::endl;
+}
+
+/*
+ * Function: isSubsequence
+ * Usage: if(isSubsequence("Positivity", "otvy") ...
+ * -------------------------------------------------
+ * Returns whether a string is a subsequence of a text or not.
+ *
+ * Implementation notes:
+ * Recursive insight, peel off characters from the text string, until and
+ * unless there is a character match between the text string's first character
+ * and subsequence (to find) string's first character. If there a match, peel
+ * off the character from subsequence string and recurse.
+ *
+ * Base case:
+ *     Empty string is the subsequence of every text.
+ *     No string is subsequence of empty string. (Except for empty string)
+ *
+ * If we run of text string with subsequence string remaining return false.
+ */
+bool isSubsequence(std::string text, std::string subsequence) {
+
+    /* Base cases */
+    if (subsequence.size() == 0) return true;
+    if (text.size() == 0 && subsequence.size() != 0) return false;
+
+    /* If first charachters match, recurse on remaining */
+    if (text[0] == subsequence[0]) {
+        return isSubsequence(text.substr(1), subsequence.substr(1));
+    }
+    /* If first charachters don't match
+     * Peel a character from the text only.
+     */
+    else {
+        return isSubsequence(text.substr(1), subsequence);
+    }
 }
