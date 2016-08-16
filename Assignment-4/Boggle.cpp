2a11a7d5c05c7cd86a33139f4765909d0b5eb5d6
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-08-16 07:42:17 +0530

    Add starter files for Assignment-4

---
+++ Assignment-4/Boggle.cpp
@@ -0,0 +1,100 @@
+/*
+ * File: Boggle.cpp
+ * ----------------
+ * This file is the main starter file for Assignment #4, Boggle.
+ */
+
+#include <iostream>
+#include "gboggle.h"
+#include "grid.h"
+#include "gwindow.h"
+#include "lexicon.h"
+#include "random.h"
+#include "simpio.h"
+using namespace std;
+
+/* Constants */
+
+const int BOGGLE_WINDOW_WIDTH = 650;
+const int BOGGLE_WINDOW_HEIGHT = 350;
+
+const string STANDARD_CUBES[16]  = {
+   "AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
+   "AOOTTW", "CIMOTU", "DEILRX", "DELRVY",
+   "DISTTY", "EEGHNW", "EEINSU", "EHRTVW",
+   "EIOSST", "ELRTTY", "HIMNQU", "HLNNRZ"
+};
+
+const string BIG_BOGGLE_CUBES[25]  = {
+   "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM",
+   "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCNSTW",
+   "CEIILT", "CEILPT", "CEIPST", "DDLNOR", "DDHNOT",
+   "DHHLOR", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU",
+   "FIPRSY", "GORRVW", "HIPRRY", "NOOTUW", "OOOTTU"
+};
+
+/* Function prototypes */
+
+void welcome();
+void giveInstructions();
+
+/* Main program */
+
+int main() {
+   GWindow gw(BOGGLE_WINDOW_WIDTH, BOGGLE_WINDOW_HEIGHT);
+   initGBoggle(gw);
+   welcome();
+   giveInstructions();
+   drawBoard(4, 4);
+   labelCube(0, 0, 'A');
+   return 0;
+}
+
+/*
+ * Function: welcome
+ * Usage: welcome();
+ * -----------------
+ * Print out a cheery welcome message.
+ */
+
+void welcome() {
+   cout << "Welcome!  You're about to play an intense game ";
+   cout << "of mind-numbing Boggle.  The good news is that ";
+   cout << "you might improve your vocabulary a bit.  The ";
+   cout << "bad news is that you're probably going to lose ";
+   cout << "miserably to this little dictionary-toting hunk ";
+   cout << "of silicon.  If only YOU had a gig of RAM..." << endl << endl;
+}
+
+/*
+ * Function: giveInstructions
+ * Usage: giveInstructions();
+ * --------------------------
+ * Print out the instructions for the user.
+ */
+
+void giveInstructions() {
+   cout << endl;
+   cout << "The boggle board is a grid onto which I ";
+   cout << "I will randomly distribute cubes. These ";
+   cout << "6-sided cubes have letters rather than ";
+   cout << "numbers on the faces, creating a grid of ";
+   cout << "letters on which you try to form words. ";
+   cout << "You go first, entering all the words you can ";
+   cout << "find that are formed by tracing adjoining ";
+   cout << "letters. Two letters adjoin if they are next ";
+   cout << "to each other horizontally, vertically, or ";
+   cout << "diagonally. A letter can only be used once ";
+   cout << "in each word. Words must be at least four ";
+   cout << "letters long and can be counted only once. ";
+   cout << "You score points based on word length: a ";
+   cout << "4-letter word is worth 1 point, 5-letters ";
+   cout << "earn 2 points, and so on. After your puny ";
+   cout << "brain is exhausted, I, the supercomputer, ";
+   cout << "will find all the remaining words and double ";
+   cout << "or triple your paltry score." << endl << endl;
+   cout << "Hit return when you're ready...";
+   getLine();
+}
+
+// [TODO: Fill in the rest of the code]
