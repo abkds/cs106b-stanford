ee9753fb10f58022d687f75fec5f46864679f32c
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-08-27 08:40:01 +0530

    Implement pqueue as doubly linked list
    Operations:
    enqueue: O(1)
    peek: O(n)
    dequeueMin: O(n)

--- Assignment-5/pqueue-doublylinkedlist.cpp
+++ Assignment-5/pqueue-doublylinkedlist.cpp
@@ -4,43 +4,85 @@
  * Implementation file for the DoublyLinkedListPriorityQueue
  * class.
  */
- 
+
 #include "pqueue-doublylinkedlist.h"
 #include "error.h"
 
 DoublyLinkedListPriorityQueue::DoublyLinkedListPriorityQueue() {
-	// TODO: Fill this in!
+	head = NULL;
+    count = 0;
 }
 
 DoublyLinkedListPriorityQueue::~DoublyLinkedListPriorityQueue() {
-	// TODO: Fill this in!
+	Cell * cp = head;
+
+    while (cp != NULL) {
+        Cell * oldCp = cp;
+        cp = cp->next;
+        delete oldCp;
+    }
 }
 
 int DoublyLinkedListPriorityQueue::size() {
-	// TODO: Fill this in!
-	
-	return 0;
+	return count;
 }
 
 bool DoublyLinkedListPriorityQueue::isEmpty() {
-	// TODO: Fill this in!
-	
-	return true;
+	return count == 0;
 }
 
 void DoublyLinkedListPriorityQueue::enqueue(string value) {
-	// TODO: Fill this in!
+	Cell * cp = new Cell;
+    cp->value = value;
+    cp->prev = NULL;
+
+    if (head == NULL) {
+        cp->next = NULL;
+    } else {
+        cp->next = head;
+        cp->next->prev = cp;
+    }
+    head = cp;
+    count++;
 }
 
 string DoublyLinkedListPriorityQueue::peek() {
-	// TODO: Fill this in!
-	
-	return "";
+	if (count == 0) error("peek: Attempting to peek an empty queue");
+
+    Cell * cp = head;
+    string minValue = head->value;
+
+    while (cp != NULL) {
+        if (cp->value < minValue) {
+            minValue = cp->value;
+        }
+        cp = cp->next;
+    }
+
+    return minValue;
 }
 
 string DoublyLinkedListPriorityQueue::dequeueMin() {
-	// TODO: Fill this in!
-	
-	return "";
-}
+	if (count == 0) error("dequeueMin: Attempting to dequeue an empty queue");
+    string value = peek();
 
+    Cell * cp = head;
+    while (cp->value != value) {
+        cp = cp->next;
+    }
+    if (cp->next == NULL && cp->prev == NULL) {
+        head = NULL;
+    } else if (cp->next == NULL && cp->prev != NULL) {
+        cp->prev->next = NULL;
+    } else if (cp->next != NULL && cp->prev == NULL) {
+        head = cp->next;
+        head->prev = NULL;
+    } else {
+        cp->prev->next = cp->next;
+        cp->next->prev = cp->prev;
+    }
+
+    count--;
+    delete cp;
+    return value;
+}
--- Assignment-5/pqueue-doublylinkedlist.h
+++ Assignment-5/pqueue-doublylinkedlist.h
@@ -17,33 +17,38 @@ class DoublyLinkedListPriorityQueue {
 public:
 	/* Constructs a new, empty priority queue backed by a doubly-linked list. */
 	DoublyLinkedListPriorityQueue();
-	
+
 	/* Cleans up all memory allocated by this priority queue. */
 	~DoublyLinkedListPriorityQueue();
-	
+
 	/* Returns the number of elements in the priority queue. */
 	int size();
-	
+
 	/* Returns whether or not the priority queue is empty. */
 	bool isEmpty();
-	
+
 	/* Enqueues a new string into the priority queue. */
 	void enqueue(string value);
-	
+
 	/* Returns, but does not remove, the lexicographically first string in the
 	 * priority queue.
 	 */
 	string peek();
-	
+
 	/* Returns and removes the lexicographically first string in the
 	 * priority queue.
 	 */
 	string dequeueMin();
 
 private:
-	// TODO: Fill this in with the implementation of your doubly-linked list
-	// priority queue.  You can add any fields, types, or methods that you
-	// wish.
+	struct Cell {
+		string value;
+		Cell * next;
+		Cell * prev;
+	};
+
+	Cell * head;
+	int count;
 };
 
 #endif
