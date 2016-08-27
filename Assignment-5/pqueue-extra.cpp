578c8925b80842fb6789e0372dd22d070bb4c4d3
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-08-27 18:09:33 +0530

    PQueue backed by Binomial Heap
    Insert and peek operations.

--- Assignment-5/pqueue-extra.cpp
+++ Assignment-5/pqueue-extra.cpp
@@ -5,43 +5,192 @@
  * do not need to implement this class, but we're happy to
  * give you extra credit if you do!
  */
- 
+
 #include "pqueue-extra.h"
 #include "error.h"
 
 ExtraPriorityQueue::ExtraPriorityQueue() {
-	// TODO: Fill this in!
+    head = NULL;
+    count = 0;
 }
 
 ExtraPriorityQueue::~ExtraPriorityQueue() {
-	// TODO: Fill this in!
+    // TODO: Fill this in!
 }
 
 int ExtraPriorityQueue::size() {
-	// TODO: Fill this in!
-	
-	return 0;
+    return count;
 }
 
 bool ExtraPriorityQueue::isEmpty() {
-	// TODO: Fill this in!
-	
-	return true;
+    return count == 0;
 }
 
 void ExtraPriorityQueue::enqueue(string value) {
-	// TODO: Fill this in!
+    Cell * cp = new Cell;
+    cp->parent = NULL;
+    cp->child = NULL;
+    cp->sibling = NULL;
+    cp->degree = 0;
+    cp->key = value;
+
+    head = binomialHeapUnion(head, cp);
+    count++;
 }
 
+/*
+ * In a binomial heap, following its properties, the root
+ * node should list should contain the minimum. Since for n
+ * elements there are at max log n trees and hence log n nodes
+ *
+ * This operation takes O(log n) time.
+ */
 string ExtraPriorityQueue::peek() {
-	// TODO: Fill this in!
-	
-	return "";
+    if (isEmpty()) error("peek: Attempting to peek an empty queue");
+    string minKey = head->key;
+
+    Cell * cp = head;
+
+    while (cp != NULL) {
+        if (cp->key < minKey) {
+            minKey = cp->key;
+        }
+        cp = cp->sibling;
+    }
+
+    return minKey;
 }
 
 string ExtraPriorityQueue::dequeueMin() {
-	// TODO: Fill this in!
-	
-	return "";
+    // TODO: Fill this in!
+
+    return "";
 }
 
+void ExtraPriorityQueue::binomialLink(Cell * & binomialTreeY, Cell * & binomialTreeZ) {
+    binomialTreeY->parent = binomialTreeZ;
+    binomialTreeY->sibling = binomialTreeZ->child;
+    binomialTreeZ->child = binomialTreeY;
+    (*binomialTreeZ).degree++;
+}
+
+Cell * ExtraPriorityQueue::binomialHeapMerge(Cell * & bHx, Cell * & bHy) {
+    if (bHx == NULL && bHy == NULL) return NULL;
+    else if (bHx != NULL && bHy == NULL) return bHx;
+    else if (bHx == NULL && bHy != NULL) return bHy;
+    else {
+        Cell * cpx = bHx;
+        Cell * cpy = bHy;
+        Cell * cpMerged = NULL;
+
+        Cell * bHMerged = new Cell;
+        bHMerged->sibling = NULL;
+        cpx = cpx->sibling;
+
+        // create the appropriate number of cells
+        while (cpx != NULL) {
+            Cell * cpm = new Cell;
+            cpm->parent = NULL;
+            cpm->sibling = bHMerged;
+            bHMerged = cpm;
+            cpx = cpx->sibling;
+        }
+
+        while (cpy != NULL) {
+            Cell * cpm = new Cell;
+            cpm->parent = NULL;
+            cpm->sibling = bHMerged;
+            bHMerged = cpm;
+            cpy = cpy->sibling;
+        }
+
+        cpx = bHx;
+        cpy = bHy;
+        cpMerged = bHMerged;
+
+        // Main merge routine
+        while (cpx != NULL && cpy != NULL) {
+            if (cpx->degree <= cpy->degree) {
+                cpMerged->degree = cpx->degree;
+                cpMerged->child = cpx->child;
+                cpMerged->key = cpx->key;
+                cpx = cpx->sibling;
+            } else {
+                cpMerged->degree = cpy->degree;
+                cpMerged->child = cpy->child;
+                cpMerged->key = cpy->key;
+                cpy = cpy->sibling;
+            }
+            cpMerged = cpMerged->sibling;
+        }
+
+        if (cpx == NULL) {
+            while (cpy != NULL) {
+                cpMerged->degree = cpy->degree;
+                cpMerged->child = cpy->child;
+                cpMerged->key = cpy->key;
+                cpy = cpy->sibling;
+                cpMerged = cpMerged->sibling;
+            }
+        }
+
+        if (cpy == NULL) {
+            while (cpx != NULL) {
+                cpMerged->degree = cpx->degree;
+                cpMerged->child = cpx->child;
+                cpMerged->key = cpx->key;
+                cpx = cpx->sibling;
+                cpMerged = cpMerged->sibling;
+            }
+        }
+
+        // Cleaning up the old root nodes
+        cpx = bHx;
+        cpy = bHy;
+
+        while (cpx != NULL) {
+            Cell * oldCp = cpx;
+            cpx = cpx->sibling;
+            delete oldCp;
+        }
+
+        while (cpy != NULL) {
+            Cell * oldCp = cpy;
+            cpy = cpy->sibling;
+            delete oldCp;
+        }
+
+        return bHMerged;
+    }
+}
+
+Cell * ExtraPriorityQueue::binomialHeapUnion(Cell * & bHx, Cell * & bHy) {
+    Cell * headM = binomialHeapMerge(bHx, bHy);
+    if (headM == NULL) return NULL;
+
+    Cell * prevX = NULL;
+    Cell * x = headM;
+    Cell * nextX = x->sibling;
+
+    while (nextX != NULL) {
+        if ((x->degree != nextX->degree) ||
+            (nextX->sibling != NULL && nextX->sibling->degree == x->degree)) {
+            prevX = x;
+            x = nextX;
+        } else if (x->key <= nextX->key) {
+            x->sibling = nextX->sibling;
+            binomialLink(nextX, x);
+        } else if (prevX == NULL) {
+            headM = nextX;
+            binomialLink(x, nextX);
+            x = nextX;
+        } else {
+            prevX->sibling = nextX;
+            binomialLink(x, nextX);
+            x = nextX;
+        }
+        nextX = x->sibling;
+    }
+
+    return headM;
+}
--- Assignment-5/pqueue-extra.h
+++ Assignment-5/pqueue-extra.h
@@ -13,40 +13,61 @@
 #include <string>
 using namespace std;
 
-/*
- * TODO: Describe this class!
+struct Cell {
+    Cell * parent, * sibling, * child;
+    int degree;
+    string key;
+};
+
+/* A class representing a priority queue backed by an
+ * binomial heap.
  */
 class ExtraPriorityQueue {
 public:
-	/* Constructs a new, empty priority queue. */
-	ExtraPriorityQueue();
-	
-	/* Cleans up all memory allocated by this priority queue. */
-	~ExtraPriorityQueue();
-	
-	/* Returns the number of elements in the priority queue. */
-	int size();
-	
-	/* Returns whether or not the priority queue is empty. */
-	bool isEmpty();
-	
-	/* Enqueues a new string into the priority queue. */
-	void enqueue(string value);
-	
-	/* Returns, but does not remove, the lexicographically first string in the
-	 * priority queue.
-	 */
-	string peek();
-	
-	/* Returns and removes the lexicographically first string in the
-	 * priority queue.
-	 */
-	string dequeueMin();
+    /* Constructs a new, empty priority queue. */
+    ExtraPriorityQueue();
+
+    /* Cleans up all memory allocated by this priority queue. */
+    ~ExtraPriorityQueue();
+
+    /* Returns the number of elements in the priority queue. */
+    int size();
+
+    /* Returns whether or not the priority queue is empty. */
+    bool isEmpty();
+
+    /* Enqueues a new string into the priority queue. */
+    void enqueue(string value);
+
+    /* Returns, but does not remove, the lexicographically first string in the
+     * priority queue.
+     */
+    string peek();
+
+    /* Returns and removes the lexicographically first string in the
+     * priority queue.
+     */
+    string dequeueMin();
 
 private:
-	// TODO: Fill this in with the implementation of your custom
-	// priority queue.  You can add any fields, types, or methods that you
-	// wish.
+    Cell * head;
+    int count;
+
+    /* Links to binomial tree of the same order.
+     * This procedure is repeatedly used by the union procedure
+     * to link together trees of same order
+     */
+    void binomialLink(Cell * & binomialTreeY, Cell * & binomialTreeZ);
+
+    /* Merges two binomial heaps according to the degree of the
+     * root nodes in both the heaps
+     */
+    Cell * binomialHeapMerge(Cell * & bHx, Cell * & bHy);
+
+    /* Returns the union of merged binomial heaps.
+     * Uses binomialHeapMerge as a subroutine
+     */
+    Cell * binomialHeapUnion(Cell * & bHx, Cell * & bHy);
 };
 
 #endif
