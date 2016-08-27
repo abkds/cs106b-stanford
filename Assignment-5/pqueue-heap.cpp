6608612ec51df0a9e7a1ed706624de8fd9eca78e
Abhishek K Das <abhishek.das.contact@gmail.com>
2016-08-27 11:05:17 +0530

    Implement PQueue as array based heap
    Operations:
    Peek: O(1)
    Enqueue: O(log n)
    Dequeue: O(log n)

--- Assignment-5/pqueue-heap.cpp
+++ Assignment-5/pqueue-heap.cpp
@@ -4,43 +4,96 @@
  * Implementation file for the HeapPriorityQueue
  * class.
  */
- 
+
+#include <algorithm>
 #include "pqueue-heap.h"
 #include "error.h"
 
 HeapPriorityQueue::HeapPriorityQueue() {
-	// TODO: Fill this in!
+    capacity = INITIAL_CAPACITY;
+	heap = new string[capacity];
+    count = 0;
 }
 
 HeapPriorityQueue::~HeapPriorityQueue() {
-	// TODO: Fill this in!
+	delete [] heap;
 }
 
 int HeapPriorityQueue::size() {
-	// TODO: Fill this in!
-	
-	return 0;
+	return count;
 }
 
 bool HeapPriorityQueue::isEmpty() {
-	// TODO: Fill this in!
-	
-	return true;
+	return count == 0;
 }
 
 void HeapPriorityQueue::enqueue(string value) {
-	// TODO: Fill this in!
+	if (count == capacity - 1) {
+        expandCapacity();
+    }
+    heap[++count] = value;
+    bubbleUp(count);
 }
 
+/*
+ * Top most position contains the smallest element by heap property
+ */
 string HeapPriorityQueue::peek() {
-	// TODO: Fill this in!
-	
-	return "";
+	if (isEmpty()) error("peek: Attempting to peek an empty queue");
+	return heap[1];
 }
 
 string HeapPriorityQueue::dequeueMin() {
-	// TODO: Fill this in!
-	
-	return "";
+	if (isEmpty()) error("dequeueMin: Attempting to dequeue an empty queue");
+    string minValue = peek();
+    heap[1] = heap[count--];
+    bubbleDown(1);
+	return minValue;
 }
 
+void HeapPriorityQueue::expandCapacity() {
+    string * oldHeap = heap;
+    capacity = 2 * capacity;
+    heap = new string[capacity];
+
+    for (int i = 1; i <= count; i++) {
+        heap[i] = oldHeap[i];
+    }
+
+    delete [] oldHeap;
+}
+
+void HeapPriorityQueue::bubbleUp(int index) {
+    if (index <= 1) return;
+
+    int parentIndex = index / 2;
+
+    if (heap[parentIndex] > heap[index]) {
+        std::swap(heap[parentIndex], heap[index]);
+        bubbleUp(parentIndex);
+    }
+}
+
+void HeapPriorityQueue::bubbleDown(int index) {
+    int leftIndex = 2 * index;
+    int rightIndex = 2 * index + 1;
+
+    if (leftIndex <= count && rightIndex <= count) {
+        int minIndex;
+
+        if (heap[leftIndex] < heap[rightIndex]) minIndex = leftIndex;
+        else minIndex = rightIndex;
+
+        if (heap[index] > heap[minIndex]) {
+            std::swap(heap[index], heap[minIndex]);
+            bubbleDown(minIndex);
+        }
+    }
+
+    if (leftIndex <= count) {
+        if (heap[index] > heap[leftIndex]) {
+            std::swap(heap[index], heap[leftIndex]);
+        }
+    }
+
+}
--- Assignment-5/pqueue-heap.h
+++ Assignment-5/pqueue-heap.h
@@ -17,36 +17,62 @@ class HeapPriorityQueue {
 public:
 	/* Constructs a new, empty priority queue backed by a binary heap. */
 	HeapPriorityQueue();
-	
+
 	/* Cleans up all memory allocated by this priority queue. */
 	~HeapPriorityQueue();
-	
+
 	/* Returns the number of elements in the priority queue. */
 	int size();
-	
+
 	/* Returns whether or not the priority queue is empty. */
 	bool isEmpty();
-	
+
 	/* Enqueues a new string into the priority queue. */
 	void enqueue(string value);
-	
+
 	/* Returns, but does not remove, the lexicographically first string in the
 	 * priority queue.
 	 */
 	string peek();
-	
+
 	/* Returns and removes the lexicographically first string in the
 	 * priority queue.
 	 */
 	string dequeueMin();
 
 private:
-	// TODO: Fill this in with the implementation of your binary heap
-	// priority queue.  You can add any fields, types, or methods that you
-	// wish.
-	//
-	// While you are free to implement this as you see fit, you *must* do
-	// all of your own memory management and should not use the Vector.
+	static const int INITIAL_CAPACITY = 10;
+
+	/*
+	 * Expands the capacity of heap if there is no more space.
+	 */
+	void expandCapacity();
+
+	/*
+	 * Heap is stored as a dynamic array with indexing starting from 1.
+	 * Element 0 has dummy value and is ignored throughout.
+	 */
+	string *heap;
+
+	int count;
+	int capacity;
+
+	/*
+	 * Bubbles down the element at top after dequeue the minimum.
+	 *
+	 * Last element in the heap is moved to top after removing the minimum
+	 * element. To restore the binary heap property, it is necessary to move
+	 * the element at top to appropriate position in heap.
+	 */
+	void bubbleDown(int index);
+
+	/*
+	 * Bubbles up a newly inserted element.
+	 *
+	 * When a new element is inserted it may disturb the heap property,
+	 * to restore the property, it is moved to the appropriate place.
+	 */
+ 	void bubbleUp(int index);
 };
 
 #endif
